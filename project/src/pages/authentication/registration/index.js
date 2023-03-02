30c16b60850d3a041d23b3fee13262ccd8271e4e
Mark Dave <dave.lorejo69@gmail.com>
2023-03-02 21:48:19 +0800

    added Modal

--- project/src/pages/authentication/registration/index.js
+++ project/src/pages/authentication/registration/index.js
@@ -1,8 +1,22 @@
+import * as React from 'react';
 import { useState } from "react"
 import axios from "axios";
-import { Card, CardContent, Grid, TextField, FormControl, Button, Link } from "@mui/material";
-import { Container } from "@mui/system";
-import { CenterFocusStrong } from "@mui/icons-material";
+import { Card, CardContent, Grid, TextField, FormControl, Button, Link, Typography, Modal, Box,  } from "@mui/material";
+import { borderRadius, Container } from "@mui/system";
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bgcolor: 'background.paper',
+    boxShadow: 2,
+    width: "30%",
+    height: "80%",
+    marginTop: "2%",
+    borderRadius: 1.3
+  };
 
 function Register() {
     const [data, setData] = useState({
@@ -11,24 +25,64 @@ function Register() {
         password: '', 
     })
     const [errorPassword, setErrorPassword] = useState('')
-  return (
-    <div style={{backgroundColor: '#F0F2F5', }}>
 
-        <Container maxwidth>
-            <Grid container spacing={2} style={{padding: 20, backgroundColor: '#F0F2F5', height: '100v'}}>
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+
+  return (
+
+<>
+        <Container maxWidth style={{alignSelf: "center", alignContents: "center"}}>
+            <Grid container spacing={2} style={{padding: 100, backgroundColor: '#F0F2F5', height: '100v'}}>
                 <Grid item xs={6}>
-                    <h1>Grid 6</h1>
+                    <CardContent style= {{marginTop:70, marginLeft:50, marginRight:50}}>
+                        <Typography gutterBottom variant="h5" component="div" style={{color:"#1877f2", fontSize: 60, fontWeight: "bolder" }}>
+                            facebook
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                        Connect with friends and the world around you on Facebook.
+                        </Typography>
+                    </CardContent>
                 </Grid>
-                <Grid item xs={6} >
-                   <Card sx={{ maxWidth: 360, }}>
+                <Grid item xs={6}  style={{alignItems: "center"}}>
+                   <Card sx={{ maxWidth: 400}} style={{marginTop: 40, marginLeft:60}} >
                         <CardContent >
                             <FormControl fullWidth a>
                                 <TextField variant="outlined" placeholder="Email" label="Email" />
                                 <TextField variant="outlined" placeholder="Password" type="password" label="Password" style={{marginTop: 15, marginBottom: 15}} />
                                 <Button variant="contained" size="large" style={{backgroundColor:"#1877f2" }}>Login</Button>
-                             
-                                <Link href="#" underline="none" style={{marginBottom: 15, marginTop: 15, justify:"center"}}>{'Forgot Password?'}</Link>
-                                <Button variant="contained" color="success" style={{backgroundColor:"#42b72a"}}>Create Account</Button>
+                                <Link href="#" underline="none" style={{marginBottom: 20, marginTop: 15,  alignSelf: "center"}}>{'Forgot Password?'}</Link>
+                                 <div style={{ display: "flex", alignItems: "center" }}>
+                                        <div style={{ flex: 1, backgroundColor: "#BDBDBD", height: "1px", marginBottom: 20}} />
+                                </div>
+                                <Button variant="contained" color="success" style={{backgroundColor:"#42b72a", marginBottom:10, marginTop:10}}
+                                    onClick={handleOpen}    >Create Account</Button>
+                                <Modal
+                                    open={open}
+                                    onClose={handleClose}
+                                    aria-labelledby="modal-modal-title"
+                                    aria-describedby="modal-modal-description">
+                                    <Box sx={style} style={{padding: 15}}>
+                                        <Typography style={{fontSize:32, fontWeight:"bold"}}>
+                                            Sign Up
+                                        </Typography>
+                                        <Typography style={{fontSize:15, color:"#525252"}}>
+                                            It’s quick and easy.
+                                        </Typography>
+                                        <div style={{ display: "flex", alignItems: "center" }}>
+                                            <div style={{ flex: 1, backgroundColor: "#BDBDBD", height: "1px", marginTop: 10}} />
+                                        </div>
+                                        <Grid item xs={6}  style={{alignItems: "center"}}>
+                                            <TextField variant="outlined" placeholder="Email" label="Email" />
+                                            </Grid>
+                                        <Grid item xs={6}  style={{alignItems: "center"}}>
+                                            <TextField variant="outlined" placeholder="Email" label="Email" />
+                                        </Grid>
+                                    </Box>
+                                </Modal>
                             </FormControl>
                             
                         </CardContent>
@@ -64,7 +118,7 @@ function Register() {
                 })
             }} >Register</button>*/}
 
-    </div>
+    </>
   )
 }
 export default Register
